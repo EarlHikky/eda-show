696a5b663420c9a3d06fb99653c13e9c650deb62
EarlHikky <andrew.malofeev@gmail.com>
2023-08-18 13:05:28 +0300

    pages_count added

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
-import time
 import json
+from concurrent.futures import ThreadPoolExecutor
+
 import requests
 from bs4 import BeautifulSoup
 
-recipes = dict()
-for page in range(1, 150):
-    url = f'https://eda.show/tag/recipe/page/{page}'
-    response = requests.get(url)
+
+def pages_count(session):
+    url = 'https://eda.show/tag/recipe/'
+    response = session.get(url)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
-    recipes_tag = soup.find('div', class_='wrapper')
-    recipes_rows_tags = recipes_tag.find_all('div', class_='row')
-    for row in recipes_rows_tags:
-        for recipe_tag in row.find_all('div', class_='col l6 m6 s12'):
-            recipe_name = recipe_tag.find('div', class_='card-limiter').h3.text
-            recipe_url = f"https://eda.show/{recipe_tag.a['href'].strip('/')}"
-            recipes.setdefault(recipe_name, recipe_url)
-    # time.sleep(1)
-
-with open('reciepes.json', 'w') as file:
-    json.dump(recipes, file)
+    pages = int(soup.find('div', class_='pagination-wrapper').text.split()[-1])
+    return pages
 
 
+def scrape_page(page, session, recipes):
+    url = f'https://eda.show/tag/recipe/page/{page}'
+    response = session.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    recipe_tags = soup.find_all('div', class_='col l6 m6 s12')
+    for recipe_tag in recipe_tags:
+        recipe_name = recipe_tag.find('div', class_='card-limiter').h3.text
+        recipe_url = f'https://eda.show/{recipe_tag.a["href"].strip("/")}'
+        recipes[recipe_name] = recipe_url
 
 
+def main():
+    recipes = dict()
+    with requests.Session() as session:
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            executor.map(lambda p: scrape_page(p, session, recipes), range(1, pages_count(session) + 1))
+
+    with open('recipes4.json', 'w') as file:
+        json.dump(recipes, file)
 
 
+if __name__ == '__main__':
+    main()
